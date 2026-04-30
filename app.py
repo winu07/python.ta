41ea66766fcd416bab462ce5f63fb48f49bc1f26
winu07 <wucindy0515@gmail.com>
2026-04-30 04:50:37 +0000

    123

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/html_tags")
+def html_tags():
+    return render_template("html_tag.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
